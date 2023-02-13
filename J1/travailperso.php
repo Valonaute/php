1ac96cbd7d7a0b497f38665ea5af8ff03b4619a8
Valonaute <v.gautier91@gmail.com>
2023-02-13 19:23:15 +0100

    Ajout travail du soir créa doc

---
+++ J1/travailperso.php
@@ -0,0 +1,21 @@
+<!-- Ouverture de la zone PHP
+avec des commentaires long en format HTML car Territoire HTML ici -->
+
+<?php
+// objectif du soir : Pratiquer sur ce qui a été vu avec des petits exos 
+
+$firstname = "Valentin" ;
+$name = "Gautier" ;
+
+/* J'affiche le prénom puis le nom 
+commentaire sur plusieurs ligne en territoire PHP */
+
+echo "Je m'appelle $firstname $name" ;
+
+
+
+
+
+
+// -------- Fin de la partie PHP ---------
+?>
