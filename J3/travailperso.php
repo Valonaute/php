b3ed7febf597ce28595f60a980c82d60c35f02c5
Valonaute <v.gautier91@gmail.com>
2023-02-16 08:53:16 +0100

    Travail a la maison

--- J3/travailperso.php
+++ J3/travailperso.php
@@ -1,6 +1,14 @@
-// Reprise de J2 
+ 
 <?php
+// Reprise de J2
+$famille = [
+    ["Valentin ", "Gautier", 31, " Opticien ", " Français" ],
+    ["Patrick ", "Gautier", 65, " Electricien ", " Français" ],
+    ["Catherine ", "Gautier", 58, " Informaticienne ", " Française" ],
+    ["Justine ", "Gautier", 34, " Diététicienne ", " Française" ]
+];
 
-
-
+foreach ($famille as $membre){
+    echo "Je m'appelle $membre[0] $membre[1], j'ai $membre[2], je suis $membre[3] et $membre[4].<br>";
+}
 ?>
